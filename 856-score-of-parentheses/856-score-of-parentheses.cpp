7c8aabf2daa7ef96fa068a9df74f3f00f964aa95
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-03-17 12:48:19 +0530

    Time: 2 ms (37.38%), Space: 6.2 MB (8.12%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        int n = s.size();
+        stack<int> st;
+        int totScore = 0;
+        vector<pair<int,int>> v(100,{0,0});
+        
+        for(int i = 0; i<n; i++){
+            if(st.empty() or s[i]=='('){
+                st.push(i);
+            }  
+            else{
+                // s[i]==')'
+                int currScore = 0;
+                if(i-st.top()==1){
+                    v[st.top()] = {i,1};
+                }
+                else{
+                    int j = st.top()+1;
+                    while(j<i){
+                        if(s[j]=='('){
+                            currScore+=v[j].second;
+                            j = v[j].first;
+                        }
+                        j++;
+                    }
+                    v[st.top()] = {i, 2*currScore};
+                }
+                st.pop();
+            }
+        }
+        int i = 0;
+        while(i<n){
+            if(s[i]=='('){
+                totScore += v[i].second;
+                i = v[i].first;
+            }
+            i++;
+        }
+        return totScore;
+    }
+};
+
+// "()(())"
+// "(()()(()))"
+// "((()())()()((()())))"
+// "((()()))"
