903e60e7a59401a42c9d68d911dfc12d348fe877
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-04-10 00:00:24 +0530

    Time: 569 ms (34.99%), Space: 89.5 MB (13.71%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     static bool cmp(vector<int> &a, vector<int> &b){
-         // return (a[1] == b[1]) ? a[0] < b[0] : a[1] < b[1];
+        // return (a[1] == b[1]) ? a[0] < b[0] : a[1] < b[1];
+        // or
         if(a[1]<b[1]) return true;
         return false;
     }
     int maxEvents(vector<vector<int>>& events) {
         int n = events.size();
         sort(events.begin(), events.end(), cmp);
-        // for(int i = 0; i<n; i++){
-        //     cout<<events[i][0]<<" "<<events[i][1]<<endl;
-        // }
         int count = 0;
         vector<int> vis(100001, 0);
         unordered_map<int,int> m;
