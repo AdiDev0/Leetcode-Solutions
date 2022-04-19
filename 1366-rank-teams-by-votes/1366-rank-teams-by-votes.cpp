843ce2676e0dbe09eb85335a2fc562acb46bc6fc
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-04-19 19:06:47 +0530

    Time: 52 ms (5.13%), Space: 13 MB (11.91%) - LeetHub

---
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    string rankTeams(vector<string>& votes) {
+        int n = votes.size();
+        unordered_map<char, vector<int>> m;
+        
+        for(int i = 0; i<n; i++){
+            string s = votes[i];
+            for(int j = 0; j<s.size(); j++){
+                if(m[s[j]].size()==0){
+                    vector<int> v(27, 0);
+                    m[s[j]] = v;
+                    m[s[j]][j+1]++;
+                }
+                else{
+                    m[s[j]][j+1]++;
+                }
+            }
+        }
+        for(auto it: m){
+            cout<<it.first<<" ";
+            vector<int> v = it.second;
+            for(int i = 1; i<v.size(); i++){
+                cout<<v[i]<<" ";
+            }
+            cout<<endl;
+        }
+        string s = "";
+        for(auto it: m){
+            s.push_back(it.first);
+        }
+        cout<<s<<" ";
+        for(int i = 0; i<s.size(); i++){
+            for(int j = i+1; j<s.size(); j++){
+                vector<int> a = m[s[i]];
+                vector<int> b = m[s[j]];
+                bool swapped = false;
+                for(int k = 1; k<=26; k++){
+                    if(a[k]<b[k]){
+                        swapped = true;
+                        swap(s[i],s[j]);
+                        break;
+                    }
+                    else if(a[k]>b[k]){
+                        swapped = true;
+                        break;
+                    }
+                }
+                if(!swapped){
+                    if(s[j]<s[i]){
+                        swap(s[i],s[j]);
+                    }
+                }
+            }
+        }
+        return s;
+    }
+};
