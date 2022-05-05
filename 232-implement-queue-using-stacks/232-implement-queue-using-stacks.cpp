a6e4c456a23eb9ce2c26826797e983fd4efeb50e
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-05-06 01:19:15 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (89.32%) - LeetHub

---
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -0,0 +1,37 @@
+class MyQueue {
+public:
+    stack<int>s;
+    stack<int>temp;
+    
+    MyQueue() {
+    }
+    
+    void push(int x) {
+        while(!s.empty()){
+            temp.push(s.top());
+            s.pop();
+        }
+        temp.push(x);
+        while(!temp.empty()){
+            s.push(temp.top());
+            temp.pop();
+        }
+    }
+    
+    /*remove the element from the front of queue and return that ele */
+    int pop() {
+        int p=s.top();
+        s.pop();
+        return p;
+    }
+    
+    /* return front element */
+    int peek() {
+        return s.top();
+    }
+    
+    /* return if queue is empty */
+    bool empty() {
+        return s.empty();
+    }
+};
