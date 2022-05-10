bdcc2b859dda9f43399a228adde46ce919325cfa
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-05-10 18:21:08 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (94.37%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve(vector<int> &v, int k, int target,vector<int> &temp, int &sum,int index){
+        if(temp.size()==k and sum==target){
+            ans.push_back(temp);
+            return;
+        }
+        if(sum>target or temp.size()>k) return;
+        int i = index;
+        while(i<9){
+            if(sum+v[i]<=target){
+                sum+=v[i];
+                temp.push_back(v[i]);
+                solve(v,k,target,temp,sum,i+1);
+                sum-=v[i];
+                temp.pop_back();
+            }
+            i++;
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> v;
+        for(int i = 1; i<=9; i++) v.push_back(i);
+        vector<int> temp;
+        int sum = 0;
+        solve(v,k,n,temp,sum,0);
+        return ans;
+    }
+};
