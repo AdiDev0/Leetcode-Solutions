b512f51a782d7a447f2a5ca64a19203998e58bf4
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-05-12 18:22:31 +0530

    Time: 867 ms (22.82%), Space: 15.9 MB (30.07%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &grid, int row, int col, vector<vector<int>> &dp){
-        int n = grid.size();
-        int m = grid[0].size();
-        
+    int solve(vector<vector<int>> &grid, int row, int col, vector<vector<int>> &dp,int n,int m){
         if(dp[row][col]!=-1) return dp[row][col];
         if(row==n-1){
             int maxi = INT_MAX;
@@ -17,7 +14,7 @@ public:
         int maxi = INT_MAX;
         for(int i = 0; i<m; i++){
             if(i!=col){
-                maxi = min(maxi, grid[row][i] + solve(grid, row+1, i, dp));
+                maxi = min(maxi, grid[row][i] + solve(grid, row+1, i, dp,n,m));
             }
         }
         dp[row][col] = maxi;
@@ -31,7 +28,7 @@ public:
         int ans = INT_MAX;
         
         for(int i = 0; i<n; i++){
-            ans = min(ans, solve(grid,1,i,dp) + grid[0][i]);
+            ans = min(ans, solve(grid,1,i,dp,n,m) + grid[0][i]);
         }
         return ans;
     }
