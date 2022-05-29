4b3519cad5d4e3e38d12c670b29aaa1d0097c902
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-05-29 15:11:24 +0530

    Time: 219 ms (26.09%), Space: 29.2 MB (13.88%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool match(vector<int> &a, vector<int> &b){
+        for(int i = 0; i<26; i++){
+            if(a[i]>0 and b[i]>0){
+                return true;
+            }
+        }
+        return false;
+    }
+    int maxProduct(vector<string>& words) {
+        int n = words.size();
+        vector<vector<int>> freq;
+        map<vector<int>, int> m;
+        
+        for(int i = 0; i<n; i++){
+            string s = words[i];
+            vector<int> v(26,0);
+            for(int j = 0; j<s.size(); j++){
+                v[s[j]-'a']++;
+            }
+            freq.push_back(v);
+        }
+        // for(int i = 0; i<n; i++){
+        //     for(auto it: freq[i]){
+        //         cout<<it<<" ";
+        //     }
+        //     cout<<endl;
+        // }    
+        int ans = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                if(!match(freq[i], freq[j])){
+                    int a = words[i].size();
+                    int b = words[j].size();
+                    ans = max(ans, a*b);
+                }
+            }
+        }
+        return ans;
+    }
+};
