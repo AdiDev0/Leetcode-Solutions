13e44952303f5e7dee0020f3c8543f3f43cedf60
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-06-04 23:28:48 +0530

    Time: 3 ms (95.17%), Space: 10 MB (12.25%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -3,9 +3,13 @@ public:
     int findNumbers(vector<int>& nums) {
         int ans = 0;
         for(auto it: nums){
-            string s = to_string(it);
-            s.size()%2==0 ? ans++: ans;
+            int k = (int)log10(it)+1;
+            k%2==0 ? ans++: ans;
         }
         return ans;
     }
 };
+// Given a Number N, we can find the number of digits in N as
+// K = floor(log10(N) + 1)
+// or
+// k = (int) log10(N)+1;
