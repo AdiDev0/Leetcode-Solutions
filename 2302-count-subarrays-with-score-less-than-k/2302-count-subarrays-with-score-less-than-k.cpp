23e8d4a5033bf7424a88bba05277bd66b1fff651
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-06-11 21:59:03 +0530

    Time: 338 ms (50.00%), Space: 95.1 MB (50.00%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.cpp
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.cpp
@@ -21,3 +21,20 @@ public:
         return ans;
     }
 };
+
+// for those who are confused why are we adding (i - j + 1) i.e cnt
+// it adds all the subarrays that ends at j starting at i both inclusive that are valid.
+// eg [1,2,3,4,5,6,7,8,9,10]
+// let i = 0, i = j;
+// total subarrays ending at j starting at i is 10
+
+// [1,2,3,4,5,6,7,8,9,10]
+// [2,3,4,5,6,7,8,9,10]
+// [3,4,5,6,7,8,9,10]
+// [4,5,6,7,8,9,10]
+// [5,6,7,8,9,10]
+// [6,7,8,9,10]
+// [7,8,9,10]
+// [8,9,10]
+// [9,10]
+// [10]
