26978683a36ad10d4543d00e7e7accfa5eea4b96
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-06-17 01:35:42 +0530

    Time: 617 ms (20.11%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -2,5 +2,7 @@
 
 select customer_number from orders
 group by customer_number
-order by count(customer_number) desc
-limit 1;
+having count(customer_number) >= all
+(select count(customer_number) from orders 
+    group by customer_number
+)
