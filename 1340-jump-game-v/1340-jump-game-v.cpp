6183f5490f3cf9f783f23f8fd30da207c7cd69c8
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-06-17 00:42:19 +0530

    Time: 42 ms (79.83%), Space: 14.9 MB (54.45%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    int solve(vector<int> &arr, int d, vector<int> &dp, int ind){
-        int n = arr.size();
+    int solve(vector<int> &arr,int n, int d, vector<int> &dp, int ind){
         if(dp[ind]!=-1) return dp[ind];
-        int lmax = arr[ind];
-        int rmax = arr[ind];
         int i = ind-1, j = ind+1;
         int l = max(ind-d,0);
         int r = min(n-1,ind+d);
         int maxi = 0;
-        while(i>=l and arr[i]<lmax){
-            maxi = max(maxi, solve(arr,d,dp,i)+1);
+        while(i>=l and arr[i]<arr[ind]){
+            maxi = max(maxi, solve(arr,n,d,dp,i)+1);
             i--;
         }
-        while(j<=r and arr[j]<rmax){
-            maxi = max(maxi, solve(arr,d,dp,j)+1);
+        while(j<=r and arr[j]<arr[ind]){
+            maxi = max(maxi, solve(arr,n,d,dp,j)+1);
             j++;
         }
         return dp[ind] = maxi;
     }
-    int maxJumps(vector<int>& arr, int d) {
+    int maxJumps(vector<int>& arr, int d){
         int n = arr.size();
         vector<int> dp(n, -1);
-        // int a = solve(arr, d, dp,0);
         for(int i = 0; i<n; i++){
-            int a = solve(arr, d, dp,i);
-            // cout<<dp[i]<<" ";
+            int a = solve(arr,n, d, dp,i);
         }
-        // cout<<endl;
         int ans = 0;
         for(int i = 0; i<n; i++){
             ans = max(ans, dp[i]);
