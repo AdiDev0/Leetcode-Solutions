4c63fe8cf345b4b61ecc0473468c3cac2f5687bd
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-06-16 16:58:24 +0530

    Time: 575 ms (19.04%), Space: 0B (100.00%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,11 +1,5 @@
 # Write your MySQL query statement below
 
-# select author_id as id from views v
-# where v.author_id in 
-# (select author_id from views vv
-#  where vv.author_id = v.author_id and vv.author_id = vv.viewer_id and                    (v.article_id<>vv.article_id or v.view_date<>vv.view_date)
-# )
-
 select distinct author_id as id from views v
 where v.author_id = v.viewer_id
 order by v.author_id;
