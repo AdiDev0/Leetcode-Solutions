29e69eb852983126580349b164fcd94e81ad9912
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-06-16 17:24:16 +0530

    Time: 129 ms (31.99%), Space: 41.8 MB (99.51%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -1,48 +1,17 @@
 class Solution {
 public:
-    bool solve(int val, vector<int> &pe, int limit){
-        int n = pe.size();
-        int high = n-1;
-        int low = 0;
-        int boats = 0;
-        
-        while(low<=high){
-            if(pe[high]+pe[low]<=limit){
-                low++;
-                high--;
-                val-=1;
-            }
-            else{
-                if(high-low==1){
-                    val-=2;
-                    break;
-                }
-                high--;
-                val--;
-            }
-        }
-        if(val<0){
-            return false;
-        }
-        return true;
-    }
-    int numRescueBoats(vector<int>& pe, int limit) {
-        int n = pe.size();
-        int low = 1;
-        int high = n;
-        
-        sort(pe.begin(), pe.end());
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int i = 0, j = people.size() - 1;
         int ans = 0;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(solve(mid, pe, limit)){
-                ans = mid;
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
-            }
+
+        while (i <= j) {
+            ans++;
+            if (people[i] + people[j] <= limit)
+                i++;
+            j--;
         }
+
         return ans;
     }
 };
