2ff044ed83443d11784b787db477742aa6b80962
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-07-03 15:26:40 +0530

    Time: 6 ms (43.60%), Space: 7.1 MB (75.45%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -6,79 +6,21 @@ public:
         for(int i = 1; i<n; i++){
             v.push_back(nums[i]-nums[i-1]);
         }
-        vector<int> dp(n-1, 0);
-        for(int i = 0; i<n-1; i++){
-            if(v[i]!=0){
-                dp[i] = 1;
-            }
-        }
         int maxPos = 0;
         int maxNeg = 0;
+        int ans = 0;
         for(int i = 0; i<n-1; i++){
+            int currAns = 0;
             if(v[i]>0){
-                dp[i] = maxNeg+1;
-                maxPos = max(maxPos, dp[i]);
+                currAns = maxNeg+1;
+                maxPos = max(maxPos, currAns);
             }
             else if(v[i]<0){
-                dp[i] = maxPos+1;
-                maxNeg = max(maxNeg, dp[i]);
+                currAns = maxPos+1;
+                maxNeg = max(maxNeg, currAns);
             }
-        }
-        int ans = 0;
-        for(auto it: dp){
-            ans = max(ans, it);
+            ans = max(ans, currAns);
         }
         return ans+1;
     }
 };
-
-
-
-
-
-// class Solution {
-// public:
-//     int solve(vector<int> &pos, vector<int> &neg){
-//         int n = pos.size();
-//         int m = neg.size();
-//         int ans = 0;
-//         for(int i = 0; i<m; i++){
-//             int val = lower_bound(pos.begin(), pos.end(), neg[i]) - pos.begin();
-//             if(i==m-1){
-//                 if(val==n) ans+=1;
-//                 else ans+=2;
-//             }
-//             else{
-//                 if(val<neg[i+1]){
-//                     ans+=2;
-//                 }
-//             }
-//         }
-//         cout<<ans<<endl;
-//         return ans+1;
-//     }
-//     int wiggleMaxLength(vector<int>& nums) {
-//         int n = nums.size();
-//         vector<int> v;
-//         for(int i = 1; i<n; i++){
-//             v.push_back(nums[i]-nums[i-1]);
-//         }
-//         vector<int> pos, neg;
-//         for(int i = 0; i<v.size(); i++){
-//             cout<<v[i]<<" ";
-//             if(v[i]>0) pos.push_back(i);
-//             if(v[i]<0) neg.push_back(i);
-//         }
-//         cout<<endl;
-//         for(auto it: pos){
-//             cout<<it<<" ";
-//         }
-//         cout<<endl;
-//         for(auto it: neg){
-//             cout<<it<<" ";
-//         }
-//         // solve(pos, neg);
-//         return max(solve(pos, neg), solve(neg,pos));
-//         // return 1;
-//     }
-// };
