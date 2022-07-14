28653fa61ff16f175569fb8439b908c8a2d075c2
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-07-14 20:58:28 +0530

    Time: 1733 ms (5.28%), Space: 333 MB (5.12%) - LeetHub

---
+++ 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.cpp
@@ -0,0 +1,55 @@
+// 0
+//  \
+//   1
+
+class Solution {
+public:
+    unordered_map<long,long> ans;
+    int solve(unordered_map<int, vector<int>> &m, int root, int nodes){
+        if(m[root].size()==0){
+            ans[nodes-1]++;
+            return 1;
+        }
+        
+        vector<int> v = m[root];
+        int l = 0;
+        int r = 0;
+        if(v.size()==2){
+            l = solve(m, v[0], nodes);
+            r = solve(m, v[1], nodes);
+            int val = (nodes-l-r-1);
+            long currAns = (long)l*r*(val!=0?val:1);
+            ans[currAns]++;
+            // cout<<root<<" "<<l<<" "<<r<<" "<<val<<endl;
+        }
+        else{
+            l = solve(m, v[0], nodes);
+            int val = (nodes-l-1);
+            long currAns = (long)l*(val!=0?val:1);
+            ans[currAns]++;
+            // cout<<root<<" "<<l<<" "<<val<<endl;
+        }
+        return l+r+1;
+    }
+    int countHighestScoreNodes(vector<int>& par) {
+        int n = par.size();
+        unordered_map<int, vector<int>> m;
+        for(int i = 0; i<n; i++){
+            m[par[i]].push_back(i);
+        }
+        int nodes = n;
+        solve(m, 0, nodes);
+        long node = -1;
+        long freq = 0;
+        for(auto it: ans){
+            if(it.first>node){
+                node = it.first;
+                freq = it.second;
+            }
+            // cout<<it.first<<" "<<it.second<<endl;
+        }
+        
+        return freq;
+    }
+};
+
