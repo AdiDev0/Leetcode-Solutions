73e31b50ae26309b207b61a32d6045795699c431
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-07-15 22:52:01 +0530

    Added solution - LeetHub

---
+++ Mobile numeric keypad - GFG/mobile-numeric-keypad.cpp
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+
+#define ll long long
+	public:
+	int ans = 0;
+	ll solve(vector<vector<int>> &v,int N, int row, int col, vector<vector<vector<ll>>> &dp){
+	    if(N==0){
+	        return 1;
+	    }
+	    if(dp[row][col][N]!=-1) return dp[row][col][N];
+	    
+	    int x[5] = {-1,0,1,0,0};
+	    int y[5] = {0,1,0,-1,0};
+	    ll count = 0;
+	    for(int i = 0; i<5; i++){
+	        int nextR = row+x[i];
+	        int nextC = col+y[i];
+	        if(nextR<4 and nextR>=0 and nextC<3 and nextC>=0 and v[nextR][nextC]!=-1){
+	            count += solve(v, N-1, nextR, nextC, dp);
+	        }
+	    }
+	    return dp[row][col][N] = count;
+	}
+	
+	long long getCount(int N){
+	    vector<vector<int>> v;
+	    v.push_back({1,2,3});
+	    v.push_back({4,5,6});
+	    v.push_back({7,8,9});
+	    v.push_back({-1,0,-1});
+	    vector<vector<vector<ll>>> dp(4, vector<vector<ll>>(4, vector<ll>(N+1, -1)));
+	    ll tot = 0;
+	    for(int i = 0; i<4; i++){
+	        for(int j = 0; j<3; j++){
+	            if(v[i][j]==-1) continue;
+	            dp[i][j][N-1] = solve(v, N-1, i, j, dp);
+	            
+	        }
+	    }
+	    for(int i = 0; i<4; i++){
+	        for(int j = 0; j<4; j++){
+	            if(dp[i][j][N-1]!=-1) tot += dp[i][j][N-1];
+	        }
+	    }
+	    return tot;
+	}
+};
+
+// { Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+       
+
+	    Solution ob;
+	    cout << ob.getCount(n) << "\n";
+	     
+    }
+    return 0;
+}
+  // } Driver Code Ends
