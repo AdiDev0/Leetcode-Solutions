84c32a3fe3285a0d5087359b2095ee65e36a1628
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-07-30 21:18:03 +0530

    Time: 214 ms (90.67%), Space: 66.6 MB (71.64%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -17,13 +17,13 @@ public:
         unordered_map<int,int> m;
         m[0] = -1;
         int ans = INT_MAX;
-        int currSum = 0;
+        long long int currSum = 0;
         for(int i = 0; i<n; i++){
-            currSum = (currSum+nums[i])%p;
-            if(m.find((currSum - sumToRemove+p)%p)!=m.end()){
-                ans = min(ans, i-m[(currSum - sumToRemove+p)%p]);
+            currSum += nums[i];
+            if(m.find((currSum - sumToRemove)%p)!=m.end()){
+                ans = min(ans, i-m[(currSum - sumToRemove)%p]);
             }
-            m[currSum] = i;
+            m[currSum%p] = i;
         }
         
         return ans>=n?-1:ans;
