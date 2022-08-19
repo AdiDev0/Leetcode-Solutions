aaf16b3c8dcb4c53f5181f514ec6d46193d5f4b4
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-08-19 21:57:23 +0530

    Time: 23 ms (43.40%), Space: 12 MB (50.16%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     int solve(vector<int> &p, int k, int buy, int ind, vector<vector<int>> &dp){
-        if(ind==p.size()-1){
-            return max(p[ind]-p[buy],0);
+        if(ind==p.size()){
+            return dp[buy][k] = 0;
         }
-        if(k<=0) return 0;
+        if(k<=0) return dp[buy][k] = 0;
         if(dp[buy][k]!=-1) return dp[buy][k];
-        int profit = 0;
+        
+        int currProfit = 0;
+        
         if(p[ind]>p[buy]){
-            int a = p[ind]-p[buy] + solve(p, k-1, ind, ind+1, dp);
-            int b = solve(p, k, buy, ind+1, dp);
-            profit = max(a,b);
+            //if curr price is greater than the price at which stock was bought
+            //we have two options
+            //a. either sell today
+            //b. keep the stock
+            int sellToday = p[ind]-p[buy] + solve(p, k-1, ind, ind+1, dp);
+            int notSellToday = solve(p, k, buy, ind+1, dp);
+            currProfit = max(sellToday, notSellToday);
         }
         else{
-            int c = solve(p, k, ind, ind+1, dp);
-            profit = c;
+            //if curr Price is lesser, then the only option is to keep the stock.
+            int notSellToday = solve(p, k, ind, ind+1, dp);
+            currProfit = notSellToday;
         }
-        return dp[buy][k] = profit;
+        return dp[buy][k] = currProfit;
     }
     int maxProfit(int k, vector<int>& p) {
         int n = p.size();
         if(n==0) return 0;
         vector<vector<int>> dp(n+1,vector<int>(k+1, -1));
-        int ans = solve(p, k, 0, 0, dp);
-        return ans;
+        return solve(p, k, 0, 0, dp);
     }
 };
