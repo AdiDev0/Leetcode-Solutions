d93bcf3fc11b1c54d64c7b3ca9b3358f8b431fd5
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-09-19 23:04:52 +0530

    Time: 267 ms (16.03%), Space: 49.7 MB (16.41%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -17,10 +17,6 @@ public:
             v.push_back(s.substr(i, j-i));
             i = j+1;
         }
-        for(auto ii: v){
-            cout<<ii<<endl;
-        }
-        // cout<<endl;
         return v;
     }
     string getPrestring(string &s){
@@ -40,7 +36,6 @@ public:
         for(int k = 0; k<n; k++){
             string it = paths[k];
             string pre = getPrestring(it);
-            // cout<<pre<<endl;
             vector<string> v = getFiles(it);
             cnt+=v.size();
             for(auto x: v){
@@ -53,18 +48,12 @@ public:
                 while(j<l and x[j]!=')'){
                     j++;
                 }
-                
                 m[x.substr(i, j-i+1)].push_back(pre+'/'+x.substr(0,l-(j-i+1)));
             }
         }
         for(auto it: m){
             if(it.second.size()<=1) continue;
             ans.push_back(it.second);
-            // cout<<it.first<<" -> ";
-            // for(auto i:it.second){
-            //     cout<<i<<" ";
-            // }
-            // cout<<endl;
         }
         if(ans.size()==cnt) return {};
         return ans;
