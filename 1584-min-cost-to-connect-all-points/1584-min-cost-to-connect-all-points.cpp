b87eb649d08a6b887f8d09c2df64bac57002e3c2
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-09-27 02:54:49 +0530

    Time: 2284 ms (9.79%), Space: 246.4 MB (5.04%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
-    static bool cmp(vector<int> &a, vector<int> &b){
-        return a[0]<=b[0];
-    }
-    int getParent(int p, unordered_map<int, int> &parent){
+    int getParent(int p, vector<int> &parent){
         if(parent[p]==p){
             return p;
         }
@@ -11,14 +8,14 @@ public:
     }
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        unordered_map<int, int> parent;
-        unordered_map<int, int> rank;
+        
+        vector<int> parent, rank;
+        
         for(int i = 0; i<n; i++){
-            parent[i] = i;
+            parent.push_back(i);
+            rank.push_back(0);
         }
-        for(int i = 0; i<n; i++){
-            rank[i] = 0;
-        }
+        
         vector<vector<int>> v;
         for(int i = 0; i<n; i++){
             for(int j = i+1; j<n; j++){
@@ -27,18 +24,13 @@ public:
                 v.push_back({a+b, i, j});
             }
         }
+        
         sort(v.begin(), v.end());
-        // for(auto it: v){
-        //     cout<<it[0]<<" "<<it[1]<<" "<<it[2]<<endl;
-        // }
+        
         int ans = 0;
         for(auto it: v){
             auto a = getParent(it[1], parent);
             auto b = getParent(it[2], parent);
-            // cout<<a<<endl;
-            // cout<<b<<endl;
-            // cout<<endl;
-            
             if(a!=b){
                 ans += it[0];
                 if(rank[a]<rank[b]){
@@ -56,10 +48,3 @@ public:
         return ans;
     }
 };
-
-// [[3,12],[-2,5],[-4,1]]
-
-
-// 6 [-2,5],[-4,1]
-// 12  [3,12],[-2,5]
-// 18 [3,12],[-4,1]
