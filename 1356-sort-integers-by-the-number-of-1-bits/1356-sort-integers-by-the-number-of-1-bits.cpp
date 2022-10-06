1e6d3f6daea2d48651c93e480f93723a4ccdbc1f
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-10-07 02:47:12 +0530

    Time: 21 ms (38.30%), Space: 10 MB (88.43%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    static bool cmp(int a, int b){
+        int A = a;
+        int B = b;
+        int cntA = 0;
+        while(a>0){
+            cntA += (a&1);
+            a = a>>1;
+        }
+        int cntB = 0;
+        while(b>0){
+            cntB += (b&1);
+            b = b>>1;
+        }
+        if(cntA==cntB) return A<B;
+        return cntA<cntB;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), cmp);
+        return arr;
+    }
+};
