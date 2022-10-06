7376f456d5b8c2950d24f335821efea5c174a841
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-10-07 02:48:18 +0530

    Time: 23 ms (31.88%), Space: 10.1 MB (69.68%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
+    // static bool cmp(int a, int b){
+    //     int A = a;
+    //     int B = b;
+    //     int cntA = 0;
+    //     while(a>0){
+    //         cntA += (a&1);
+    //         a = a>>1;
+    //     }
+    //     int cntB = 0;
+    //     while(b>0){
+    //         cntB += (b&1);
+    //         b = b>>1;
+    //     }
+    //     if(cntA==cntB) return A<B;
+    //     return cntA<cntB;
+    // }
     static bool cmp(int a, int b){
         int A = a;
         int B = b;
         int cntA = 0;
         while(a>0){
-            cntA += (a&1);
-            a = a>>1;
+            a = a&(a-1);
+            cntA++;
         }
         int cntB = 0;
         while(b>0){
-            cntB += (b&1);
-            b = b>>1;
+            b = b&(b-1);
+            cntB++;
         }
         if(cntA==cntB) return A<B;
         return cntA<cntB;
