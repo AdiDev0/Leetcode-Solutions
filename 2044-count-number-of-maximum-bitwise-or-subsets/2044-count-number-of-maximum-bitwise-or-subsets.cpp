68475a404ed092063dc6b15ed60f1f45b3e4dc42
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-10-09 20:49:24 +0530

    Time: 20 ms (91.12%), Space: 8.3 MB (83.55%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -3,38 +3,24 @@ public:
     int maxi = -1;
     int freq = 0;
     int n = 0;
-    vector<vector<int>> ans;
-    void solve(vector<int> &nums, int i, int curr, vector<int> &v){
+    void solve(vector<int> &nums, int i, int curr){
         if(i==n){
             if(curr>maxi){
                 maxi = curr;
                 freq = 1;
-                ans.clear();
-                ans.push_back(v);
             }
             else if(curr==maxi){
                 freq++;
-                ans.push_back(v);
             }
             return;
         }
-        v.push_back(nums[i]);
-        solve(nums, i+1, curr|nums[i], v);
-        v.pop_back();
-        solve(nums, i+1, curr, v);
+        solve(nums, i+1, curr|nums[i]);
+        solve(nums, i+1, curr);
     }
     int countMaxOrSubsets(vector<int>& nums) {
         n = nums.size();
         vector<int> v;
-        solve(nums, 0, 0, v);
-        // cout<<maxi<<endl;
-        // for(auto it: ans){
-        //     for(auto i: it){
-        //         cout<<i<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        // cout<<endl;
+        solve(nums, 0, 0);
         return freq;
     }
 };
