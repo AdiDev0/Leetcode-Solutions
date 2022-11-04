d9ace30e5cae14824819228d078f715c2bcee4c6
AdiDev0 <85286921+AdiDev0@users.noreply.github.com>
2022-11-04 01:16:42 -0700

    Time: 0 ms (100.00%), Space: 6.8 MB (12.37%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int minMutation(string start, string end, vector<string>& bank) {
-	//st holds all valid mutations
+        
 	unordered_set<string> st{bank.begin(),bank.end()};
-	//if end mutaion is not in list return -1;
+	
 	if(!st.count(end)) return -1;
-	//start BFS by pushing the starting mutation
+	
 	queue<string> Q;
 	Q.push(start);
 	int steps=0,s;
@@ -15,12 +15,10 @@ public:
 		while(s--){
 			cur=Q.front();
 			Q.pop();
-			//If we reach end mutation
+			
 			if(cur==end) return steps;
-			//We erase the cur mutation in order to avoid redundant checking
+			
 			st.erase(cur);
-			//as the length of mutation is 8 and it can take A,C,G,T
-			//at each index we check the possibility of mutation by replcaing it with A,C,G,T
 			for(int i=0;i<8;i++){
 				t=cur;
 				t[i]='A';
